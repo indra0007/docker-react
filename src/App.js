6954f7e18cbb324928e7b180508eb5b2aa6b527b
Supratim Kundu <Supratim.Kundu@wu.com>
2019-04-16 14:31:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and sa Modified AMi janina
           </p>
           <a
             className="App-link"
